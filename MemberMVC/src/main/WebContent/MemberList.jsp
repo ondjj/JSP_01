133b21b48ffc1f5c5e05423f0cddbce642e50048
ondjj <jcjk0302@gmail.com>
2022-10-18 22:55:11 +0900

    MemberJoin to Jsp Model2

---
+++ MemberMVC/src/main/WebContent/MemberList.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<div align="center">
+	<h2>모든 회원 보기</h2>
+	
+	<table style="width:800" border="1" bordercolor="gray">
+		<tr height="40">
+			<td width="50" align="center">아이디</td>
+			<td width="200" align="center">이메일</td>
+			<td width="150" align="center">전화</td>
+			<td width="150" align="center">취미</td>
+			<td width="150" align="center">직업</td>
+			<td width="100" align="center">나이</td>
+		</tr>
+		<c:forEach var="bean" items="${arr}">
+		
+		<tr height="40">
+			<td width="50" align="center">${bean.id }</td>
+			<td width="200" align="center"><a href="#">${bean.email }</a></td>
+			<td width="150" align="center">${bean.tel }</td>
+			<td width="150" align="center">${bean.hobby }</td>
+			<td width="150" align="center">${bean.job }</td>
+			<td width="100" align="center">${bean.age }</td>
+		</tr>
+		
+		</c:forEach>
+	
+	</table>
+	
+</div>
+
+</body>
+</html>
---
+++ MemberMVC/src/main/java/control/MemberJoinProc.java
@@ -0,0 +1,77 @@
+package control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.MemberBean;
+import model.MemberDAO;
+
+/**
+ * Servlet implementation class MemberJoinProc
+ */
+@WebServlet("/proc.do")
+public class MemberJoinProc extends HttpServlet {
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		reqPro(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		reqPro(request, response);
+
+	}
+	
+	protected void reqPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//한글 처리
+		request.setCharacterEncoding("UTF-8");
+		
+		MemberBean bean = new MemberBean();
+		bean.setId(request.getParameter("id"));
+		String pass1 = request.getParameter("pass1");
+		String pass2 = request.getParameter("pass2");
+		bean.setPass1(pass1);
+		bean.setPass2(pass2);
+		bean.setEmail(request.getParameter("email"));
+		bean.setTel(request.getParameter("tel"));
+		
+		String[] arr = request.getParameterValues("hobby");
+		
+		String data ="";
+		for (String string : arr) {
+			data += string + " "; // 하나의 스트링으로 데이터 연결
+		}
+		
+		bean.setHobby("data");
+		bean.setJob(request.getParameter("job"));
+		bean.setAge(request.getParameter("age"));
+		bean.setInfo(request.getParameter("info"));
+		
+		// 패스워드가 같을 경우에만 데이터베이스에 저장
+		if(pass1.equals(pass2)) {
+			
+			// 데이터 베이스 객체 선언한 후 저장
+			MemberDAO mdao = new MemberDAO();
+			mdao.insertMember(bean);
+			
+			//컨트롤에서 또 다른 컨트롤러를 호출해주어야한다.
+			
+			RequestDispatcher dis = request.getRequestDispatcher("MemberListCon.do");
+			dis.forward(request, response);
+			
+		}else {
+			
+			request.setAttribute("msg", "패스워드가 일치 하지 않습니다.");
+			RequestDispatcher dis = request.getRequestDispatcher("LoginError.jsp");
+			dis.forward(request, response);
+		}
+	}
+
+}
